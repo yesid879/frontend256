a36ac880e47cf0f333792118ba1ad0842a19c5c7
yquintero.andes <yquintero.andes@gmail.com>
2024-10-30 21:51:31 -0500

    primer commit

---
+++ js/request.js
@@ -0,0 +1,11 @@
+//const url = 'http://localhost:7000/api/';
+const url = 'https://backend256.onrender.com/api/';
+
+function sendRequest(endPoint, method, data){
+let request = new XMLHttpRequest();
+request.open(method, url+endPoint);
+request.responseType = 'json';
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(data ? JSON.stringify(data): data);
+return request
+}
